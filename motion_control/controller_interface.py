e93557bbd1c2b73a77111bbf961b00f99c362124
elimoss <mossy173@gmail.com>
2025-04-05 20:28:25 -0700

    some tuning

--- motion_control/controller_interface.py
+++ motion_control/controller_interface.py
@@ -16,13 +16,16 @@ class MotionController:
         # steps per mm
         self.run_gcode('M92 X80 Y80 Z80 E100', silent=True)
         # set max acceleration
-        self.run_gcode(f'M201 X1 Y6000 Z4500 E5000', silent=True)
+        # remember, z is actually x, y is y, x is actually z
+        self.run_gcode(f'M201 X3000 Y2000 Z4000 E5000', silent=True)
         # acceleration
-        self.run_gcode(f'M204 P10000 T10000', silent=True)
+        self.run_gcode(f'M204 P30000 T30000', silent=True)
         # max feedrate
-        self.run_gcode('M203 X1 Y1000 Z650 E5000', silent=True)
+        self.run_gcode('M203 X200 Y300 Z500 E5000', silent=True)
         # motor current
-        self.run_gcode('M906 X1500 Z1800 Y1500', silent=True)
+        self.run_gcode('M906 X1800 Z1500 Y1800', silent=True)
+        # reverse Y
+        # self.run_gcode('M907 X1
 
     def run_gcode(self, gcode: str, silent: bool = False):
         if not gcode.endswith('\r\n'):
@@ -33,18 +36,18 @@ class MotionController:
     def __run_and_wait_for_completion(self, gcode: bytes, silent: bool):
         self.connection.write(gcode)
         # wait 200ms
-        time.sleep(0.1)
+        # time.sleep(0.1)
         if not silent:
             print(self.connection.readline().decode().strip())
         self.connection.write(b"M400\r\n")
-        time.sleep(0.1)
+        # time.sleep(0.1)
         while True:
             response = self.connection.readline().decode().strip()
             if not silent:
                 print('\t'.join([gcode.decode(), response]))
             if response == "ok":
                 break
-            time.sleep(0.1)
+            # time.sleep(0.1)
 
     def home(self, home_x=True, home_y=True, home_z=False):
         print("Homing....")
@@ -52,11 +55,11 @@ class MotionController:
         # touch off all axis limit switches
         cmd = 'G28'
         if home_x:
-            cmd += ' X0'
+            cmd += ' Z0'
         if home_y:
             cmd += ' Y0'
         if home_z:
-            cmd += ' Z0'
+            cmd += ' X0'
         if not any([home_x, home_y, home_z]):
             print("No axis to home")
             return
@@ -66,15 +69,38 @@ class MotionController:
     def disable_steppers(self):
         self.run_gcode("M84")
 
-    def jog(self, x, y, feedrate=None):
+    def jog(self, *, x=None, y=None, z=None, feedrate=None):
+        if x is None and y is None and z is None:
+            print("No axis to jog")
+            return
         # Z is driving X because the controller board has dual z outputs and i have dual x motors.
-        # Y is Y. there is no Z or E.
-        self.run_gcode(f"G1 Z{x} Y{y} F{feedrate or self.default_feedrate}")
+        # Y is Y. z is driven by x.
+        self.run_gcode(f"G1 X{z} Y{y} Z{x}  F{feedrate or self.default_feedrate}")
+
+    def pickup(self, z_height, feedrate=None):
+        self.jog(z=z_height, feedrate=feedrate or self.default_feedrate)
+        self.jog(z=10)
+
+    def set_feedrate(self, feedrate):
+        self.default_feedrate = feedrate
 
 
 if __name__ == "__main__":
-    ctrl = MotionController()
-    feedrate = 60000
-    ctrl.jog(400, 300, feedrate=feedrate)
-    ctrl.jog(0, 0, feedrate=feedrate)
+    ctrl = MotionController(feedrate=40000)
+    ctrl.home(home_x=True, home_y=True, home_z=True)
+    ctrl.jog(x=200)
+    ctrl.jog(y=150, x=300)
+    ctrl.pickup(60)
+    ctrl.jog(y=200, x=100)
+    ctrl.pickup(60)
+    ctrl.jog(x=300)
+    ctrl.pickup(60)
+    ctrl.jog(x=5, y=5)
+    ctrl.pickup(60)
+
+
+    # for _ in range(1):
+    #     ctrl.jog(10, 0, 0)
+        # ctrl.jog(40, 40, 40, feedrate=feedrate)
+
     ctrl.disable_steppers()
