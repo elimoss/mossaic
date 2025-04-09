4f6da87ff35801835ccc6ff9014023a04e53f2de
elimoss <mossy173@gmail.com>
2025-04-08 20:22:33 -0700

    does the tuning go on? yes it does.

--- motion_control/controller_interface.py
+++ motion_control/controller_interface.py
@@ -16,11 +16,11 @@ class MotionController:
         # steps per mm
         self.run_gcode('M92 X80 Y80 Z80', silent=True)
         # set max acceleration
-        self.run_gcode(f'M201 X1000 Y800 Z4000 E5000', silent=True)  # x stage acceleration is high due to a resonance issue
+        self.run_gcode(f'M201 X4000 Y5000 Z4000 E5000', silent=True)  # x stage acceleration is high due to a resonance issue
         # acceleration
         self.run_gcode(f'M204 P30000 T30000', silent=True)
         # max feedrate
-        self.run_gcode('M203 X200 Y500 Z700', silent=True)
+        self.run_gcode('M203 X1000 Y500 Z700', silent=True)
         # motor current
         self.run_gcode('M906 X1800 Z1800 Y1800', silent=True)
         # override z limit
@@ -87,8 +87,8 @@ class MotionController:
         self.run_gcode(cmd + f" F{feedrate or self.default_feedrate}")
 
     def pickup(self, z_height, feedrate=None):
-        self.jog(z=z_height, feedrate=feedrate or self.default_feedrate)
-        self.jog(z=10)
+        self.jog(z=z_height)
+        self.jog(z=10, feedrate=feedrate or self.default_feedrate)
 
     def set_feedrate(self, feedrate):
         self.default_feedrate = feedrate
@@ -98,25 +98,36 @@ if __name__ == "__main__":
     ctrl = MotionController(feedrate=40000)
     # ctrl.home(home_x=True, home_y=True, home_z=True)
 
-    ctrl.jog(x=100, feedrate=30000)
-    ctrl.jog(x=10, feedrate=30000)
-    ctrl.jog(x=500, y=250)
-    ctrl.jog(x=300)
-    ctrl.pickup(60)
-    ctrl.jog(y=150, x=300)
-    ctrl.pickup(60)
-    ctrl.jog(y=200, x=100)
-    ctrl.pickup(60)
-    ctrl.jog(x=300)
-    ctrl.pickup(60)
-    ctrl.jog(x=5, y=5)
-    ctrl.pickup(60)
+    if True:
+        for _ in range(1):
+            ctrl.pickup(60, feedrate=4000)
 
-    for _ in range(1):
-        # ctrl.set_feedrate(15000)
-        ctrl.jog(x=500, y=250)
+    if True:
+        # a series of single-axis movements
+        ctrl.jog(x=100)
+        ctrl.jog(x=10)
+        ctrl.jog(x=500)
         ctrl.jog(x=300)
+        ctrl.jog(y=100)
+        ctrl.jog(x=100)
+        ctrl.pickup(60)
+        ctrl.jog(y=150)
+        ctrl.jog(x=50)
+        ctrl.pickup(60)
+        ctrl.jog(y=200)
+        ctrl.jog(x=250)
+        ctrl.pickup(60)
+        ctrl.jog(x=300)
+        ctrl.jog(y=100)
+        ctrl.pickup(60)
+        ctrl.pickup(60)
 
-    ctrl.jog(x=50, y=50)
+    if True:
+        for _ in range(2):
+            ctrl.jog(x=500)
+            ctrl.jog(x=300)
+            ctrl.jog(y=10)
+            ctrl.jog(y=250)
+    # ctrl.jog(x=50, y=50)
 
     ctrl.disable_steppers()
